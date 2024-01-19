ab18378dde8a83cf8d55b94b3b5d15e68c6cb254
Kyle Yang <kylesyang4128@gmail.com>
2024-01-18 23:09:05 -0800

    coding is done

---
+++ w2/main.js
@@ -0,0 +1,20 @@
+const houseHoldMembers = 4;
+const houseSize = 7;
+const foodChoices = 14;
+const waterConsumption = 1;
+const houseHoldPurchases = 10;
+const wasteProduction = 20;
+const wasteRecycle = 0;
+const annualTransport = 14;
+
+
+const total = document.querySelector("h2");
+total.textContent = 
+houseHoldMembers + 
+houseSize + 
+foodChoices + 
+waterConsumption + 
+houseHoldPurchases + 
+wasteProduction + 
+wasteRecycle + 
+annualTransport;
