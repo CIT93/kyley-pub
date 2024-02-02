fb892158ae1686694a4ff35fb00c889264c20ae5
Kyle Yang <kylesyang4128@gmail.com>
2024-02-01 23:04:07 -0800

    updated output to DOM

--- w4/main.js
+++ w4/main.js
@@ -43,16 +43,44 @@ function start(houseHoldMembers, houseSize) {
 
 
 function displayOutput() {
-  
+  for (arr of cfpData){
+    console.log(arr);
+    const output = document.getElementById("output");
+    const newP = document.createElement("p");
+    newP.textContent = `Carbon Footprint total is ${arr[4]}.
+    The number of household members is ${arr[0]}. Which gives is a carbon footprint score of ${arr[2]}.
+    The size of the house is ${arr[1]}. Which gives is a carbon footprint score of ${arr[3]}.`
+    output.appendChild(newP);
+  }
 }
 
+start(7, "apartment");
+start(7, "large");
+start(7, "medium");
+start(7, "small");
+start(6, "apartment");
+start(6, "large");
+start(6, "medium");
+start(6, "small");
 start(5, "apartment");
 start(5, "large");
 start(5, "medium");
 start(5, "small");
+start(4, "apartment");
 start(4, "large");
+start(4, "medium");
+start(4, "small");
+start(3, "apartment");
+start(3, "large");
 start(3, "medium");
+start(3, "small");
+start(2, "apartment");
+start(2, "large");
+start(2, "medium");
 start(2, "small");
+start(1, "apartment");
+start(1, "large");
+start(1, "medium");
 start(1, "small");
 
 displayOutput()
