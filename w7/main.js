ca291b0866478b88716b48f12f11075956989d3e
Kyle Yang <kylesyang4128@gmail.com>
2024-02-26 21:40:41 -0800

    my attempt at the code, before I review other students code

--- w7/main.js
+++ w7/main.js
@@ -1,42 +1,9 @@
-import { renderTbl } from "./render.js";
+import { renderTbl, renderTblHeading } from "./render.js";
+import { determineHouseHoldPts, determineHouseSizePts } from "./cfp.js";
 
 const FORM = document.getElementById("form");
 const OUTPUT = document.getElementById("output");
 const cfpData = [];
-
-function determineHouseSizePts(size) {
-  let houseSizePoints = 0;
-  if (size === "large") {
-    houseSizePoints = 10;
-  } else if (size === "medium") {
-    houseSizePoints = 7;
-  } else if (size === "small") {
-    houseSizePoints = 4;
-  } else if (size === "apartment") {
-    houseSizePoints = 2;
-  }
-  return houseSizePoints;
-}
-
-function determineHouseHoldPts(numberInHousehold) {
-  let houseHoldPoints = 0;
-  if (numberInHousehold === 1) {
-    houseHoldPoints = 14;
-  } else if (numberInHousehold === 2) {
-    houseHoldPoints = 12;
-  } else if (numberInHousehold === 3) {
-    houseHoldPoints = 10;
-  } else if (numberInHousehold === 4) {
-    houseHoldPoints = 8;
-  } else if (numberInHousehold === 5) {
-    houseHoldPoints = 6;
-  } else if (numberInHousehold === 6) {
-    houseHoldPoints = 4;
-  } else if (numberInHousehold > 6) {
-    houseHoldPoints = 2;
-  }
-  return houseHoldPoints;
-}
 
 function start(houseHoldMembers, houseSize) {
   const houseHoldPTS = determineHouseHoldPts(houseHoldMembers);
@@ -51,24 +18,7 @@ function start(houseHoldMembers, houseSize) {
     houseSPTS: houseSizePTS,
     cfpTotal: total,
   });
-
 }
-
-// function displayOutput() {
-//   for (obj of cfpData){
-//     console.log(obj)
-//     const newH2 = document.createElement("h2");
-//     newH2.textContent = `Carbon Footprint is ${obj.cfpTotal} of ${obj.firstName} ${obj.lastName}`;
-//     const newH3 = document.createElement("h3");
-//     newH3.textContent = `Based on number in and size of home`;
-//     const newP = document.createElement("p");
-//     newP.textContent = `This number is based on the number of people in the house of ${obj.houseM} (score: ${obj.houseMPTS})`;
-//     newP.textContent += ` and a ${obj.houseS} size of home (score: ${obj.houseSPTS}).`;
-//     OUTPUT.appendChild(newH2);
-//     OUTPUT.appendChild(newH3);
-//     OUTPUT.appendChild(newP);
-//   }
-// }
 
 FORM.addEventListener('submit', function(e){
   e.preventDefault();
@@ -78,14 +28,7 @@ FORM.addEventListener('submit', function(e){
   const houseSize = FORM.houses.value;
   start(houseMembers, houseSize);
   OUTPUT.innerHTML = "";
-  // displayOutput();
+  renderTblHeading();
   renderTbl(cfpData);
   FORM.reset();
 })
-
-// Does the apartment score correct? If not why not?
-// Yes the apartment score was correct, because the string that was being
-// searched for was correct whereas the video they are abbrviated "apt".
-
-// Why are we doing all this work in the form to make sure the gives us good data?
-// Because you have to assume the user will give bad data which can cause coding issues.
--- w7/render.js
+++ w7/render.js
@@ -1,10 +1,11 @@
 const TBL = document.getElementById("tab-data")
 
 function renderTblHeading() {
+    TBL.innerHTML = "";
     const table = document.createElement("table");
     const thead = document.createElement("thead");
     const tr = document.createElement("tr");
-    const headingTextArr = ["Name", "HouseHold", "HouseSize", "Footprint", "Action"];
+    const headingTextArr = ["First", "Last", "HouseHold", "HouseSize", "Footprint"];
     headingTextArr.forEach(function(text){
       const th = document.createElement("th");
       th.textContent = text;
@@ -12,30 +13,32 @@ function renderTblHeading() {
     });
     thead.appendChild(tr);
     table.appendChild(thead);
-    return table
+    return table;
   }
-  
+
   function renderTbl(data) {
     const table = renderTblHeading();
     const tbody = document.createElement("tbody");
-    const tr = document.createElement("tr");
-    const trTextArr = ["Kyle Yang", 5, "Large", 6]
-    trTextArr.forEach(function(text){
-      const td = document.createElement("td");
-      td.textContent = text;
-      tr.appendChild(td);
-    })
-    const td = document.createElement("td");
-    const btnEdit = document.createElement("button");
-    const btnDel = document.createElement("button");
-    btnEdit.textContent = "Edit";
-    btnDel.textContent = "Del";
-    td.appendChild(btnEdit);
-    td.appendChild(btnDel);
-    tr.appendChild(td);
-    tbody.appendChild(tr);
+    data.forEach(function (item) {
+      const tr = document.createElement("tr");
+      const tdArr1 = document.createElement("td");
+      const tdArr2 = document.createElement("td");
+      const tdArr3 = document.createElement("td");
+      const tdArr4 = document.createElement("td");
+      const tdArr5 = document.createElement("td");
+      tdArr1.textContent = item.firstName;
+      tdArr2.textContent = item.lastName;
+      tdArr3.textContent = item.houseMPTS;
+      tdArr4.textContent = item.houseSPTS;
+      tdArr5.textContent = item.cfpTotal;
+      tr.appendChild(tdArr1);
+      tr.appendChild(tdArr2);
+      tr.appendChild(tdArr3);
+      tr.appendChild(tdArr4);
+      tr.appendChild(tdArr5);
+      tbody.appendChild(tr);
+    });
     table.appendChild(tbody);
-    console.log(table);
     TBL.appendChild(table);
   }
 
