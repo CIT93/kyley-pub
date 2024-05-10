7ba99b3da20696655f25e0908b80400b191bf380
Kyle Yang <kylesyang4128@gmail.com>
2024-05-09 21:30:39 -0700

    final

---
+++ final/storage.js
@@ -0,0 +1,19 @@
+import { cfpData } from "./global.js";
+
+const saveLS = () => {
+    const serializedArr = JSON.stringify(cfpData);
+    localStorage.setItem("cfp", serializedArr);
+}
+
+const getLS = () => {
+    const retrievedArr = localStorage.getItem("cfp");
+    if(retrievedArr !== null) {
+        return JSON.parse(retrievedArr);
+    } else {
+        return [];
+    }
+}
+
+
+
+export {cfpData, saveLS, getLS};
